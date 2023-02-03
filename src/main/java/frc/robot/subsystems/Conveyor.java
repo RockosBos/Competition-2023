3ca9c30ac830a80e7ea37f00da5dc59c57e7c721
RockosBos <32206608+RockosBos@users.noreply.github.com>
2023-02-03 10:52:34 -0500

    Partially completed Descriptions

--- src/main/java/frc/robot/subsystems/Conveyor.java
+++ src/main/java/frc/robot/subsystems/Conveyor.java
@@ -4,8 +4,31 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+//This class will control the conveyor belt that serves the purpose of positioning cubes and cones so that they can be grabbed by the lift.
+
+/*
+ * Components *
+ *  Conveyor Motor(14) - Spark Max Canbus powering Neo 550.
+ *  Intake Photoeye(0) - Digital Input sensor located on the front of the robot.
+ *  Lift Photoeye(1) - Digital Input sensor located on the back of the robot.
+ * 
+ * Description *
+ *  This subsystem by default will not be controlled by the driver or operator (outside manual control).
+ *  Instead the intake sensor will detect if a game piece has entered the conveyor and the conveyor will move until
+ *  the game piece has reached the lift side sensor.
+ * 
+ * Commands *
+ *  setConveyor (Default) - Runs the functionality described above.
+ *  setManualForward - Manually runs the conveyor forward.
+ *  setManualBackward - Manually runs the conveyor backward.
+ * 
+ * Functions *
+ *  setConveyor
+ *  setConveyorManual
+ *  get
+ */
 
 public class Conveyor extends SubsystemBase {
   /** Creates a new Conveyor. */
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -6,6 +6,23 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+//This subsystem will control the intake arm that work to bring a game piece into the robot. This includes the extension and the intake rollers.
+
+/*
+ * Components *
+ *  intakeRoller Motor (15) - Spark Max Canbus powering a NEO 550.
+ *  intakeExtend Motor (16) - Spark Max Canbus powering a NEO.
+ *  intakeExtendLimit (2) - Digital Input limit switch.
+ *  intakeRetractLimit (3) - Digital Input limit switch.
+ * 
+ * Description *
+ *  The intake will extend out until the intake extend ls is pressed. Then the rollers will begin spinning.
+ *  It will retract once the intake button is no longer being pressed. It will be required 
+ * 
+ * Commands *
+ *  
+ */
+
 public class Intake extends SubsystemBase {
   /** Creates a new Intake. */
   public Intake() {}
